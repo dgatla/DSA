cd2e660acafa0fd8b37a4319ab3b1e79ee17f952
dgatla <dinessh.ga@outlook.com>
2024-11-03 19:58:57 +0530

    isSequence 324

---
+++ 392. Is Subsequence/main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    bool isSubsequence(string s, string t)
+    {
+        int i = 0, j = 0;
+        while (i < s.length() and j < t.length())
+        {
+            if (s[i] == t[j])
+            {
+                i++;
+            }
+            j++;
+        }
+        return i == s.length();
+    }
+};
